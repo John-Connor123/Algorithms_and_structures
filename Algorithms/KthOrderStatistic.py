4cf2a59f270b23c62894b3a09d79d1248c3b8dc7
John-Connor123 <ivan.eudokimoff2014@gmail.com>
2024-02-04 17:57:48 +0300

    Finished writing deterministic select algorithm.

--- Algorithms/KthOrderStatistic.py
+++ Algorithms/KthOrderStatistic.py
@@ -1,4 +1,4 @@
-from Sort import Sort
+from Algorithms.Sort import Sort
 from random import randint
 
 
@@ -7,16 +7,18 @@ class KthOrderStatistics:
     __k = None
 
     @classmethod
-    def k_order_statistics(cls, lst: list, k: int, randomized: bool = False):
+    def k_order_statistics(cls, lst: list, k: int, randomized: bool = True):
         """
-        Based on deterministic select (median of medians) algorithm by default. Sometimes it called BFPRT because of the last names of the authors.
-        Using with randomized=True will choose random select (random pivot pick) algorithm.
-        Complexity of both algorithms: O(n)
+        Based on random select (random pivot pick) algorithm by default.
+        Using with randomized=False will choose deterministic select (median of medians) algorithm.
+        Sometimes it called BFPRT because of the last names of the authors.
+        Complexity of both algorithms: O(n).
+        Memory-complexity of random select is O(n), but memory-complexity of deterministic select is O(n*log_n).
         """
         cls.__lst = lst.copy()
-        cls.__k = k
+        cls.__k = k - 1
         return cls.__randomised_k_order_statistics(0, len(cls.__lst)-1) if randomized \
-            else cls.__determined_k_order_statistics(0, len(cls.__lst)-1, lst=cls.__lst)
+            else cls.__determined_k_order_statistics(0, len(cls.__lst)-1)
 
     @classmethod
     def __randomised_k_order_statistics(cls, left, right):
@@ -27,8 +29,10 @@ class KthOrderStatistics:
         m = cls.__split(left, right)
         if cls.__k < m:
             return cls.__randomised_k_order_statistics(left, m-1)
+        elif cls.__k == m:
+            return cls.__lst[m]
         else:
-            return cls.__randomised_k_order_statistics(m, right)
+            return cls.__randomised_k_order_statistics(m+1, right)
 
     @classmethod
     def __determined_k_order_statistics(cls, left, right, lst=None, k=None):
@@ -38,33 +42,26 @@ class KthOrderStatistics:
             k = cls.__k
         if lst is None:
             lst = cls.__lst
-        print(f"Start_loop: left: {left}, right: {right}, lst: {lst}, k={k}")
-        if k == 0:
-            return lst[k]
         if left >= right:
             return lst[left]
 
+        m = cls.__split(left, right, pivot=cls.__pick_pivot(lst), lst=lst)
+        if k < m:
+            return cls.__determined_k_order_statistics(0, m-1-left, k=k, lst=lst[left:m])
+        elif k == m:
+            return lst[k]
+        else:
+            return cls.__determined_k_order_statistics(0, right-m-1, k=k-m-1, lst=lst[m+1:right+1])
+
+    @classmethod
+    def __pick_pivot(cls, lst):
+        if len(lst) == 1:
+            return lst[0]
+
         for i in range(0, len(lst), 5):
             lst[i:i+5] = Sort.quick_sort(lst[i:i+5])
-        median_lst = lst[2:len(lst):5] if len(lst) > 2 else lst[0:1]
-        print("Median size:", len(median_lst), ", median_lst:", median_lst, end='\n\n')
-
-        pivot = cls.__determined_k_order_statistics(0, len(median_lst)-1, lst=median_lst, k=len(median_lst)//2)
-        print("pivot:", pivot)
-
-
-        m = cls.__split(left, right, pivot=pivot, lst=lst) - 1
-        print(f"m={m}, k={k}, left={left}, right={right}")
-        print(f'End_loop: left={left}, right={right}, m={m}, k={k}, pivot={pivot}, lst={lst}')
-        if k < m:
-            print(f"k({k}) < m({m})")
-            return cls.__determined_k_order_statistics(left, m-1, k=k, lst=lst)
-        elif k == m:
-            print(f"k({k})=m({m}), lst[k]={lst[k]}, pivot={pivot}")
-            return pivot
-        else:
-            print(f"k({k}) >= m({m})")
-            return cls.__determined_k_order_statistics(m, right, k=k, lst=lst)
+        median_lst = lst[2:len(lst):5] if len(lst) >= 5 else [lst[randint(0, len(lst)-1)]]
+        return cls.__pick_pivot(lst=median_lst)
 
     @classmethod
     def __split(cls, left, right, pivot=None, lst=None):
@@ -73,33 +70,31 @@ class KthOrderStatistics:
             lst = cls.__lst
         if pivot is None:
             pivot = lst[randint(left, right)]
-        while left <= right:
-            while lst[left] < pivot:
-                left += 1
-            while lst[right] > pivot:
-                right -= 1
+        ind = lst.index(pivot)
+        lst[left], lst[ind] = lst[ind], lst[left]
 
-            if left <= right:
-                lst[left], lst[right] = lst[right], lst[left]
-                left += 1
-                right -= 1
-        return left
+        m = left
+        for i in range(left+1, right+1):
+            if lst[i] <= lst[left]:
+                m += 1
+                lst[i], lst[m] = lst[m], lst[i]
+
+        lst[left], lst[m] = lst[m], lst[left]
+        return m
 
 
 if __name__ == '__main__':
-    # print("Description of method k_order_statistics: ", KthOrderStatistics.k_order_statistics.__doc__)
+    print("Description of method k_order_statistics: ", KthOrderStatistics.k_order_statistics.__doc__)
     iterations_count = 100
     array_count = 10000
 
     for i in range(1, iterations_count+1):
         if i % 10 == 0:
             print(f"Iteration {i}/{iterations_count}")
-        lst = [randint(-2*array_count, 2*array_count) for _ in range(array_count)]
-        k = randint(0, array_count-1)
+        lst = [randint(-3*array_count, 3*array_count) for _ in range(array_count)]
+        k = randint(1, array_count)
 
-        lst1 = KthOrderStatistics.k_order_statistics(lst, k)
-        print(f"k = {k}", lst1, sorted(lst)[k])
-        assert sorted(lst)[k] == lst1
-        assert sorted(lst)[k] == KthOrderStatistics.k_order_statistics(lst, k, randomized=True)
+        assert sorted(lst)[k-1] == KthOrderStatistics.k_order_statistics(lst, k, randomized=True)
+        assert sorted(lst)[k-1] == KthOrderStatistics.k_order_statistics(lst, k, randomized=False)
 
     print("\nTests passed!")
